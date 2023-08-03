019e9338f4d696e6939fe8a7de7343ee9ca1c01c
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2023-08-03 19:12:16 +0530

    #Added Error Handler

--- controllers/constactControllers.js
+++ controllers/constactControllers.js
@@ -1,24 +1,33 @@
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
+
+
 const getContacts = (req, res) => {
     res.status(200).json({ message: "Get all contacts" });
 };
 
 
-const createContact = (req, res) => {
-    console.log(req.body)
+const createContact = asyncHandler(async (req, res) => {
+    console.log("The request body is",req.body)
+    const {name, email, phone} = req.body;
+    if(!name || !email || !phone){
+        res.status(400);
+        throw new Error("All fields are mandotory")
+    }
     res.status(201).json({ message: "Create contact" });
-};
+});
 
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Get contact for ${req.params.id}` });
-};
+});
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update contact for ${req.params.id}` });
-};
+});
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete contact for ${req.params.id}` });
-};
+});
 
 
 
